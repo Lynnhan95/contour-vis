6e12012e772b412c1e493b1ea48ce8b92a712c49
SchubyDong <>
2019-11-27 15:33:51 -0500

    temp-change

--- src/components/map.js
+++ src/components/map.js
@@ -221,15 +221,62 @@ class BaseMap extends Component {
                         let prev = arr[i]
                         let next = arr[i + 1]
                         let thetaX = (next[0] - prev[0])
+						let flag
                         let thetaY = (next[1] - prev[1])
                         let theta_Virtical = thetaX / thetaY
+
+						let median = [[(next[0] + prev[0]) / 2, (next[1] + prev[1]) / 2], [(next[0] + prev[0]) / 2 + 0.1, (next[1] + prev[1]) / 2 -  0.1 * theta_Virtical] ]
                         
-                        let median = [[(next[0] + prev[0]) / 2, (next[1] + prev[1]) / 2], [(next[0] + prev[0]) / 2 + 0.1 , (next[1] + prev[1]) / 2 -  0.1 * theta_Virtical] ]
                         MedianVertical.push(median)
                     }
                 }
                 getMedialVertical(interpolateArr)
                 console.log(MedianVertical)
+				/*
+
+				function btwn(a, b1, b2) {
+  					if ((a >= b1) && (a <= b2)) { return true; }
+					if ((a >= b2) && (a <= b1)) { return true; }
+  					return false;	
+				}
+
+				function line_line_intersect(line1, line2) {
+				  var x1 = line1.x1, x2 = line1.x2, x3 = line2.x1, x4 = line2.x2;
+				  var y1 = line1.y1, y2 = line1.y2, y3 = line2.y1, y4 = line2.y2;
+				  var pt_denom = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+				  var pt_x_num = (x1*y2 - y1*x2) * (x3 - x4) - (x1 - x2) * (x3*y4 - y3*x4);
+				  var pt_y_num = (x1*y2 - y1*x2) * (y3 - y4) - (y1 - y2) * (x3*y4 - y3*x4);
+				  if (pt_denom == 0) { return "parallel"; }
+				  else { 
+					var pt = {'x': pt_x_num / pt_denom, 'y': pt_y_num / pt_denom}; 
+					if (btwn(pt.x, x1, x2) && btwn(pt.y, y1, y2) && btwn(pt.x, x3, x4) && btwn(pt.y, y3, y4)) { return pt; }
+					else { return "not in range"; }
+				  }
+				}
+
+				function path_line_intersections(pathEl, line) {
+
+				  var pts = []
+				  for (var i=0; i<n_segments; i++) {
+					var pos1 = pathEl.getPointAtLength(pathLength * i / n_segments);
+					var pos2 = pathEl.getPointAtLength(pathLength * (i+1) / n_segments);
+					var line1 = {x1: pos1.x, x2: pos2.x, y1: pos1.y, y2: pos2.y};
+					var line2 = {x1: line.attr('x1'), x2: line.attr('x2'), 
+								 y1: line.attr('y1'), y2: line.attr('y2')};
+					var pt = line_line_intersect(line1, line2);
+					if (typeof(pt) != "string") {
+					  pts.push(pt);
+					}
+				  }
+
+				  return pts;
+
+				}
+				*/
+				
+				
+				
+				
 
                 const verticalLines = MedianVertical.map((d,i) => {
                     return (
