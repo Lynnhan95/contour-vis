ac578385d1b92ac0c71369c4bf8b5fe7a4566201
Dadatata-JZ <zhao835@purdue.edu>
2020-02-14 18:47:00 -0500

    rounding new setting

--- src/components/getDensity.js
+++ src/components/getDensity.js
@@ -21,7 +21,7 @@ export function getDensity (svg,pts_ary,segment_num = 5000){
 
 
   var strPath = getLinePathStr(pts_ary)
-  strPath = roundPathCorners(strPath,0.1,true)
+  strPath = roundPathCorners(strPath,0.18,true)
 
   var p = svg.append("path")
                   .style("fill","none")
